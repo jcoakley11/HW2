d032a5fc285d9ca708c2a2488b531602a196eb0e
jcoakley11 <jcoakley@bu.edu>
2016-11-14 19:20:05 -0500

    Add files via upload

---
+++ MealTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  EC 601 HW2
+//
+//  Created by John Coakley on 10/1/16.
+//  Copyright © 2016 John Coakley. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    // MARK: Properties
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
